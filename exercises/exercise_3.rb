8cbe06ffbb10260e9dab8c393a83f73461f35ae0
Gregory Dechant <dechantg@gmail.com>
2023-11-14 00:50:32 +0000

    completed exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,15 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+
+@store3 = Stores.find(3)
+
+p @store3
+
+@store3.destroy
+
+
+stores_count = Stores.count
+puts "after the destruction of store 3 the count is now #{stores_count}"
+
