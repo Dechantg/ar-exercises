d014abbe7c163dd36fd43c6e2a1dbf8f39f1eeb9
Gregory Dechant <dechantg@gmail.com>
2023-11-14 01:11:54 +0000

    completed exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,23 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+
+total_revenu = Stores.sum(:annual_revenue)
+store_count = Stores.count
+average_revenue = total_revenu / store_count
+@over_target = Stores.where('annual_revenue > ?', 1000000)
+
+
+puts "Total revenue from year is: #{total_revenu}"
+puts "Average store revenue is: #{average_revenue}"
+puts "Stores that exceeded $1 Million in revenue"
+@over_target.each do |store|
+  puts "#{store.name} had annual revenue of #{store.annual_revenue}"
+end
+
+
+
+
+
+
