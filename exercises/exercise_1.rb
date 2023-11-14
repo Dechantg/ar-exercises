cf13329d81be143837120b6e85bcf514d3a1237d
Gregory Dechant <dechantg@gmail.com>
2023-11-14 03:57:58 +0000

    finished exercise 7. moved validations into exercise 1 file for the class definitions. also added validation loop to the input in exercise 7 for the boolean inputs on appearl to make sure a value is selected

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,13 +5,39 @@ puts "----------"
 
 # Your code goes below here ...
 
+module StoreValidations
+extend ActiveSupport::Concern
+
+included do
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, presence: true, numericality: { greater_than: 0 }
+end
+
+end
+
+module EmployeeValidations
+extend ActiveSupport::Concern
+
+included do
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :store_id, presence: true
+  validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+end
+end
+
 class Stores < ActiveRecord::Base
+  include StoreValidations
+
   has_many :employees, class_name: 'Employee', foreign_key: 'store_id'
 end
 
 class Employee < ActiveRecord::Base
+  include EmployeeValidations
+
   belongs_to :store, class_name: 'Stores', foreign_key: 'id'
 end
+
 
 stores = Stores.new
 stores.name = "Burnaby"
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,66 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+
+
+
+print "Enter a new store name: "
+new_store_name = gets.chomp
+
+print "Enter Annual Revenue: "
+new_store_revenue = gets.chomp.to_i
+
+
+
+print "Does the store carry men's apparel? Yes/No: "
+new_store_men_apparel = nil
+
+loop do
+  input = gets.chomp.downcase
+
+  if input == "yes"
+    new_store_men_apparel = true
+    break
+  elsif input == "no"
+    new_store_men_apparel = false
+    break
+  else
+    puts "Invalid input. Please enter 'Yes' or 'No'."
+    print "Does the store carry men's apparel? Yes/No: "
+  end
+end
+
+print "Does the store carry women's apparel? Yes/No: "
+new_store_women_apparel = nil
+
+loop do
+  input = gets.chomp.downcase
+
+  if input == "yes"
+    new_store_women_apparel = true
+    break
+  elsif input == "no"
+    new_store_women_apparel = false
+    break
+  else
+    puts "Invalid input. Please enter 'Yes' or 'No'."
+    print "Does the store carry women's apparel? Yes/No: "
+  end
+end
+
+
+
+stores = Stores.new
+stores.name = new_store_name
+stores.annual_revenue = new_store_revenue
+stores.mens_apparel = new_store_men_apparel
+stores.womens_apparel = new_store_women_apparel
+if stores.save
+  puts "Store saved successfully!"
+else
+  puts "Error saving store:"
+  stores.errors.full_messages.each do |error|
+    puts error
+  end
+end
